117f9d8fb8b17b3e04aabbdd7fb149547f7fcaaa
Javier Navarro <javinavarrorobles@gmail.com>
2025-02-14 11:03:16 +0100

    gestor de errores

--- src/server.js
+++ src/server.js
@@ -11,12 +11,30 @@ import { UPLOADS_DIR, FRONTEND_HOST } from '../env.js';
 export const server = express();
 
 /* MIDLEWARES */
-server.use(morgan('dev')); 
+server.use(morgan('dev'));
 server.use(express.json());
-server.use(fileupload()); 
-const uploadsDir = path.join(process.cwd(), `src/${UPLOADS_DIR}`); 
-server.use('/uploads', express.static(uploadsDir)); 
-server.use(cors(/* { origin: FRONTEND_HOST } */)); 
+server.use(fileupload());
+const uploadsDir = path.join(process.cwd(), `src/${UPLOADS_DIR}`);
+server.use('/uploads', express.static(uploadsDir));
+server.use(cors(/* { origin: FRONTEND_HOST } */));
 
 /* ROUTERS */
 server.use(router);
+
+/* errores */
+
+
+
+
+//gestor de errores
+
+server.use((error, req, res, next) => {
+    console.error(error);
+    //Manda la respuesta predefinida
+    res.status(error.httpStatus || 500).send({
+        httpStatus: error.httpStatus || 500,
+        status: 'ERROR!!!',
+        code: error.code || 'INTERNAL _SERVER_ERROR',
+        message: error.message
+    });
+}); 
