cc11d3a94986cb2a84ecec197d0c07f242fb608b
Javier Navarro <javinavarrorobles@gmail.com>
2025-02-14 11:43:44 +0100

    todo testeado

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+
 import { server } from './src/server.js';
 
 import { PORT } from './env.js';
+
 
 const puerto = PORT || 3001;
 
 server.listen(puerto, () => {
 	console.log(`Servidor escuchando en el puerto: ${puerto}`);
 });
+
--- env.js
+++ env.js
@@ -5,6 +5,9 @@ export const {
 	MYSQL_USER,
 	MYSQL_PASSWORD,
 	MYSQL_DATABASE,
-	MYSQL_PORT
-	
+	MYSQL_PORT,
+	FRONTEND_HOST,
+	UPLOADS_DIR,
+	PORT
+
 } = process.env;
