5b31aec9ff3fef6ad5a67855367ddb81f2f9ff9c
Dámaris Mercado <damiemergencia@gmail.com>
2025-02-15 16:46:41 +0100

    Quité algunos comentarios

--- src/middlewares/uploadMiddleware.js
+++ src/middlewares/uploadMiddleware.js
@@ -2,15 +2,14 @@ import fileUpload from 'express-fileupload';
 import path from 'path';
 import fs from 'fs';
 
-// Definir la carpeta de almacenamiento
+
 const uploadDir = process.env.UPLOADS_DIR || 'uploads';
 
-// Crear la carpeta si no existe
+
 if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir, { recursive: true });
 }
 
-// Middleware de subida de archivos
 export const uploadMiddleware = (req, res, next) => {
     if (!req.files || Object.keys(req.files).length === 0) {
         return res.status(400).json({ error: 'No se ha subido ningún archivo' });
@@ -20,13 +19,13 @@ export const uploadMiddleware = (req, res, next) => {
     const fileName = `${Date.now()}_${file.name}`;
     const filePath = path.join(uploadDir, fileName);
 
-    // Mover el archivo a la carpeta definida
+    
+    
     file.mv(filePath, (err) => {
         if (err) {
             return res.status(500).json({ error: 'Error al subir el archivo', details: err });
         }
 
-        req.filePath = filePath; // Guardar la ruta en el request
-        next();
+        req.filePath = filePath; 
     });
 };
