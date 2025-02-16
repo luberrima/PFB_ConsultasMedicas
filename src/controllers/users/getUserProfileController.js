bc0859e811cb221d235c873d0be2cab4127a6ffc
Franollie <franbejaranom@gmail.com>
2025-02-16 19:05:00 +0100

    creo rama y subo info usuario

---
+++ src/controllers/users/getUserProfileController.js
@@ -0,0 +1,22 @@
+import {selectUserByIdModel} from '../../models/users/selectUserByIdModel.js';
+
+export const getUserProfileController = async (requestAnimationFrame, resizeBy, next) => {
+    try {
+        const { userid } = req.params;
+
+        const user = await selectUserByIdModel(userId);
+
+        delete user.password;
+        delete user.registrationCode;
+        delete user. recoveryPassCode;
+
+        res.send({
+            status: 'ok',
+            data: {
+                user,
+            },
+        });
+    } catch (error) {
+        next(error);
+    }
+};
---
+++ src/models/users/selectUserByIdModel.js
@@ -0,0 +1,16 @@
+import { getPool } from "../../db/getPool";
+import { genereErrorUtils } from "../../utils/genereErrorUtils";
+
+export const selectUserByIdModel = async (userId) => {
+    const pool = await getPool();
+
+    const [users] = await pool.query(
+        `SELECT id, username, nombre, email, password, role, avatar, bio, active, registrationCode, recoveryPassCode, createdAt FROM users WHERE id = ?`, [userId]
+    );
+
+    if (users.length === 0) {
+        throw genereErrorUtils('El usuario no existe');
+    }
+
+    return users[0];
+};
