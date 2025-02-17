c665c1ce22ceb245f4c7e5b4a5462968fcd0ba54
lucia.franco <lucia.beatriz.franco@gmail.com>
2025-02-17 23:08:49 +0100

    añadir endpoint a usersRouter

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -2,9 +2,11 @@ import express from 'express';
 import { registerUserController } from '../controllers/users/registerUserController.js';
 import { registerDoctorController } from '../controllers/users/registerDoctorController.js';
 import { sendValidationEmailController } from '../controllers/users/sendValidationEmailController.js';
+import { getAllDoctorsController } from '../controllers/users/getAllDoctorsController.js';
 
 export const usersRouter = express.Router();
 
 usersRouter.post('/users/register', registerUserController);
 usersRouter.post('/users/register-doctor', registerDoctorController);
 usersRouter.post('/users/send-validation-email', sendValidationEmailController);
+usersRouter.post('/users/doctors', getAllDoctorsController);
