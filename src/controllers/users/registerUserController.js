ee79827dbf90546587b3e5282743a0195616db66
lucia.franco <lucia.beatriz.franco@gmail.com>
2025-02-17 22:46:13 +0100

    voler a actualziar imports

--- src/controllers/users/registerUserController.js
+++ src/controllers/users/registerUserController.js
@@ -1,10 +1,10 @@
 import { newUserSchema } from '../../schemas/users/newUserSchema.js';
 import { registerUserService } from '../../services/users/registerUserService.js';
-import { validateSchemaUtils } from '../../utils/validateSchemaUtil.js';
+import { validateSchemaUtil } from '../../utils/validateSchemaUtil.js';
 
 export const registerUserController = async (req, res, next) => {
     try {
-        await validateSchemaUtils(newUserSchema, req.body);
+        await validateSchemaUtil(newUserSchema, req.body);
         const { username, email, password } = req.body;
         const user = await registerUserService(username, email, password);
         res.status(201).send({
