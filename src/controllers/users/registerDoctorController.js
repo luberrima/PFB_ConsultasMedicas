bb10854a8b4ca9a07dcf0cf68dc0f1205f301f02
lucia.franco <lucia.beatriz.franco@gmail.com>
2025-02-17 21:40:05 +0100

    actualizar joi en register-doctor

--- src/controllers/users/registerDoctorController.js
+++ src/controllers/users/registerDoctorController.js
@@ -1,11 +1,26 @@
 import { newDoctorSchema } from '../../schemas/users/newDoctorSchema.js';
 import { registerDoctorService } from '../../services/users/registerDoctorService.js';
+import { validateSchemaUtils } from '../../utils/validateSchemaUtil.js';
 
 export const registerDoctorController = async (req, res, next) => {
     try {
-        await newDoctorSchema.validateAsync(req.body);
-        const { username, email, password, collegeNumber, dateOfCollege, skillId } = req.body;
-        const doctor = await registerDoctorService(username, email, password, collegeNumber, dateOfCollege, skillId);
+        await validateSchemaUtils(newDoctorSchema, req.body);
+        const {
+            username,
+            email,
+            password,
+            collegeNumber,
+            dateOfCollege,
+            skillId,
+        } = req.body;
+        const doctor = await registerDoctorService(
+            username,
+            email,
+            password,
+            collegeNumber,
+            dateOfCollege,
+            skillId
+        );
         res.status(201).send({
             status: 'ok',
             message: 'Doctor registrado correctamente',
