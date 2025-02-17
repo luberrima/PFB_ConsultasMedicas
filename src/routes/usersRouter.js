1431d77e380c888978ea49022e10d5f583126c40
Rafael <vianneyrlm@gmail.com>
2025-02-17 16:59:38 +0100

    Merge de la linea de login

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -2,14 +2,15 @@ import express from 'express';
 import { registerUserController } from '../controllers/users/registerUserController.js';
 import { registerDoctorController } from '../controllers/users/registerDoctorController.js';
 import { sendValidationEmailController } from '../controllers/users/sendValidationEmailController.js';
-
-export const usersRouter = express.Router();
-
-usersRouter.post('/users/register', registerUserController);
-usersRouter.post('/users/register-doctor', registerDoctorController);
-usersRouter.post('/users/send-validation-email', sendValidationEmailController);import express from 'express';
 import { loginUserController } from '../controllers/users/loginUserController.js';
-
 export const usersRouter = express.Router();
 
 usersRouter.post('users/login', loginUserController);
+usersRouter.post('/users/register', registerUserController);
+usersRouter.post('/users/register-doctor', registerDoctorController);
+usersRouter.post('/users/send-validation-email', sendValidationEmailController);import express from 'express';
+
+
+
+
+
