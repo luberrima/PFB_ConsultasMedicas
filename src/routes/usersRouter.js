823bb1b6def3ca8ba43acafead71b3f0e7b44bcc
Rafael <vianneyrlm@gmail.com>
2025-02-17 17:09:21 +0100

    Comentamos linea faltante

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,16 +1,12 @@
 import express from 'express';
 import { registerUserController } from '../controllers/users/registerUserController.js';
 import { registerDoctorController } from '../controllers/users/registerDoctorController.js';
-import { sendValidationEmailController } from '../controllers/users/sendValidationEmailController.js';
+/*import { sendValidationEmailController } from '../controllers/users/sendValidationEmailController.js';*/
 import { loginUserController } from '../controllers/users/loginUserController.js';
+
 export const usersRouter = express.Router();
 
 usersRouter.post('users/login', loginUserController);
 usersRouter.post('/users/register', registerUserController);
 usersRouter.post('/users/register-doctor', registerDoctorController);
-usersRouter.post('/users/send-validation-email', sendValidationEmailController);import express from 'express';
-
-
-
-
-
+/*usersRouter.post('/users/send-validation-email', sendValidationEmailController);  Falta el archivo utils sendEmailBrevoUtil.js  */ 
