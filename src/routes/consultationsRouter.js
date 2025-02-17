660efc2d704b199914aa885b63361527a0a49be3
VikingRacer89 <alanso89@gmail.com>
2025-02-17 11:18:47 +0100

    Endpoint de creacion de respuestas

--- src/routes/consultationsRouter.js
+++ src/routes/consultationsRouter.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { getConsultationDetailController } from '../controllers/consultations/getConsultationDetailController.js';
+import { createReplyController } from '../controllers/consultations/createReplyController.js';
 
 export const consultationsRouter = express.Router();
 
 consultationsRouter.get('/consultations/:consultationId', getConsultationDetailController);
+consultationsRouter.post('/consultations/:consultationId/replies', createReplyController);
