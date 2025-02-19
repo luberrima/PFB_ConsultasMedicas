d6681f7b4d259923b64d0ade6c95badc9e22209d
lucia.franco <lucia.beatriz.franco@gmail.com>
2025-02-19 17:20:18 +0100

    actualizar imports

--- src/controllers/users/activeUserController.js
+++ src/controllers/users/activeUserController.js
@@ -1,26 +1,25 @@
 import { activeUserService } from '../../services/users/activeUserService.js';
 
 export const activeUserController = async (req, res, next) => {
+    try {
+        const { registrationCode } = req.params;
+        // Comprobar si hay código de registro
+        if (!registrationCode) {
+            throw genereErrorUtils(
+                400,
+                'REGISTRATION_CODE_MISSING',
+                'El código de registro es obligatorio'
+            );
+        }
 
-	try {
-		const { registrationCode } = req.params;
-		// Comprobar si hay código de registro
-		if (!registrationCode) {
-			throw generateErrorUtils(
-				400,
-				'REGISTRATION_CODE_MISSING',
-				'El código de registro es obligatorio'
-			);
-		}
+        const user = await activeUserService(registrationCode);
 
-		const user = await activeUserService(registrationCode);
-
-		res.status(200).send({
-			status: 'ok',
-			message: 'Usuario activado. Inicia sesión',
-			data: user,
-		});
-	} catch (error) {
-		next(error);
-	}
+        res.status(200).send({
+            status: 'ok',
+            message: 'Usuario activado. Inicia sesión',
+            data: user,
+        });
+    } catch (error) {
+        next(error);
+    }
 };
