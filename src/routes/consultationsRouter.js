9a251dbb8dcf48aff817c73bb9e7fecde326ddac
lucia.franco <lucia.beatriz.franco@gmail.com>
2025-02-20 22:33:50 +0100

    cambio del nombre de la ruta de new-consultation para consistencia gramatical con las demas rutas

--- src/routes/consultationsRouter.js
+++ src/routes/consultationsRouter.js
@@ -1,15 +1,31 @@
 import express from 'express';
 import { getConsultationDetailController } from '../controllers/consultations/getConsultationDetailController.js';
 import { createReplyController } from '../controllers/consultations/createReplyController.js';
-import { authUserMiddleware } from '../middlewares/authUserMiddleware.js'
+import { authUserMiddleware } from '../middlewares/authUserMiddleware.js';
 import { voteDoctorController } from '../controllers/consultations/voteDoctorController.js';
 import { newConsultController } from '../controllers/consultations/newConsultController.js';
 import { getAllConsultationController } from '../controllers/consultations/getAllConsultationController.js';
 
 export const consultationsRouter = express.Router();
 
-consultationsRouter.get('/consultations/:consultationId', authUserMiddleware, getConsultationDetailController);
-consultationsRouter.post('/consultations/:consultationId/replies', authUserMiddleware, createReplyController);
-consultationsRouter.post('/consultations/:id/vote', authUserMiddleware, voteDoctorController);
-consultationsRouter.post('/consult', authUserMiddleware, newConsultController);
-consultationsRouter.get('/consultations',getAllConsultationController);
+consultationsRouter.get(
+    '/consultations/:consultationId',
+    authUserMiddleware,
+    getConsultationDetailController
+);
+consultationsRouter.post(
+    '/consultations/:consultationId/replies',
+    authUserMiddleware,
+    createReplyController
+);
+consultationsRouter.post(
+    '/consultations/:id/vote',
+    authUserMiddleware,
+    voteDoctorController
+);
+consultationsRouter.post(
+    '/new-consultation',
+    authUserMiddleware,
+    newConsultController
+);
+consultationsRouter.get('/consultations', getAllConsultationController);
