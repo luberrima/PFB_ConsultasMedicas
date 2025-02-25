b18012e4ae25be828a3e87da4c76d0819eb657e7
Javier Navarro <javinavarrorobles@gmail.com>
2025-02-25 19:45:37 +0100

    esqueleto de la lanfing page y fotos de recurso

--- client/src/components/Landing/CarruselDoctor.jsx
+++ client/src/components/Landing/CarruselDoctor.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import { CardDoctor } from './CardDoctor.jsx'
+import { Button } from '../Button.jsx';
+import { Icon } from '../Icon.jsx';
+
+
+const handleClickNext = () => {
+  setCurrentPhoto((prev) => (prev + 1) % photos.length);
+};
+const handleClickPrev = () => {
+  setCurrentPhoto((prev) => (prev - 1 + photos.length) % photos.length);
+};
 
 export const CarruselDoctor = () => {
   return (
     <>
-     <CardDoctor/> 
-     <CardDoctor/> 
-     <CardDoctor/> 
+    
+        <Button className='prev' handleClick={handleClickPrev}>
+						<Icon name='chevron_left' />
+				</Button>
+					<CardDoctor/> 
+          <CardDoctor/> 
+          <CardDoctor/> 
+				<Button className='next' handleClick={handleClickNext}>
+						<Icon name='chevron_right' />
+				</Button>
+
     </>
   )
 }
