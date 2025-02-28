adbaee892ab4a1a3ef756b42b85799833a0dfa82
Javier Navarro <javinavarrorobles@gmail.com>
2025-02-28 10:01:13 +0100

    arreglo de fotos en la landing

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,12 @@
 import { Button } from "../components/Button.jsx";
 import { Icon } from "../components/Icon.jsx";
+
 import { CarruselDoctor } from "../components/Landing/CarruselDoctor.jsx";
 import { useNavigate } from "react-router-dom";
+
+import logo from "../assets/good-doctor-logo.svg"; // Logo
+import equipo from "../assets/Fotomedicos.png"; // Equipo medico
+import famila from "../assets/madrehijotablet.jpg"; // familiatablet
 
 
 export const HomePage = () => {
@@ -20,7 +25,7 @@ export const HomePage = () => {
     
     <>
     <header>
-      <img src= '../assets/good-doctor-logo.svg' alt='logo Good Doctor'/>
+      <img src= {logo} alt='logo Good Doctor'/>
       <h2>PAGINA MENU</h2>
     </header>
     <main>
@@ -28,13 +33,13 @@ export const HomePage = () => {
       <CarruselDoctor />
       <section>
         <article>
-          <img src ='../assets/fotomedicos.png' alt ="Foto de equipo medico" />
+          <img src ={equipo} alt ="Foto de equipo medico" />
           <h3>Equipo medico a ti disposición</h3>
           <p>Un lugar al que pertenecer, en el que puedes encontrar una red amplia de médicos a tu disposición y elección, ya que puedes ver su ratio de satisfacción de sus diagnósticos. </p>
 
         </article>
         <article>
-        <img src ='../assets/madrehijotablet.jpg' alt ="Foto de madre he hijo con una tablet" />
+        <img src ={famila} alt ="Foto de madre he hijo con una tablet" />
           <h3>En cualquier momento y lugar</h3>
           <p>Puedes encontrar diagnóstico de médicos sobre tus dolencias y todo a unos pocos clips. Puedes formar parte de nuestra comunidad, ver tu historial por si algún dolor o duda regresa. Todo ello en un entorno cómodo, intuitivo y responsive, puede verlo en tu Tablet, ordenador o teléfono.  </p>
         </article>
