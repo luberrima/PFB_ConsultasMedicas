f499a5938b7952e51404a6561cac5bae9f6fbf86
Javier Navarro <javinavarrorobles@gmail.com>
2025-03-01 23:57:53 +0100

    page de todos los medicos y landing con el carrusel de los medios mostrando tres

--- client/src/services/fetchBackEnd.js
+++ client/src/services/fetchBackEnd.js
@@ -1,10 +1,30 @@
 const backEndPath = import.meta.env.VITE_BACKEND_HOST;
 
+
 export const getOwnUserService = async (token) => {
-    const response = await fetch(`${backEndPath}/users/own`, {
+    const response = await fetch(`${backEndPath}/users/profile`, {
         headers: {
-            Authorization: `Bearer ${token}`,
+            Authorization: `${token}`,
         },
     });
     return response;
+};
+
+
+export const getAllDoctorsService = async () => {
+    console.log('estoy dentro de getAllDoctorService');
+    
+
+	const response = await fetch(`${backEndPath}/users/doctors`);
+    
+
+	const { message, data } = await response.json();
+
+	if (!response.ok) throw new Error(message);
+
+
+    console.log('esto es lo que trae el getAllDoctorsService', data); 
+    
+	return data;
+
 };
