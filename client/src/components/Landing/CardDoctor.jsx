6dcf75c181bbfcab71a12755f5e41cb9eecc939d
lucia.franco <lucia.beatriz.franco@gmail.com>
2025-03-03 20:05:37 +0100

    comentarios para evitar errores

--- client/src/components/Landing/CardDoctor.jsx
+++ client/src/components/Landing/CardDoctor.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import avatardefault from '../../assets/avatar-default.png';
-//import { Icon } from '../Icon.jsx';
-
-
+const staticPath = import.meta.env.VITE_BACKEND_STATIC;
 export const CardDoctor = ({ doctor }) => {
     /* console.log('Esto es lo que tiene la CardDoctor para pintar de doctores',doctor);
   console.log('Esto es lo que tiene Cardoctor doctores.name',doctor.username); */
+    /* const entryPhotoPath = `${staticPath}/a/${doctor.userId}/${doctor.id}`; */
+    console.log(`${staticPath}/avatars/${doctor.userId}/${doctor.avatar}`);
 
     return (
         <>
             <li className="card-doctor-inicio">
-                <img src={avatardefault} alt="avatar-default" />
-                <h3 className="doctor-name">{doctor.username}</h3>
-                <p className="doctor-skill">{doctor.skillId}</p>
+                <img
+                    src={`${staticPath}/avatars/${doctor.userId}/${doctor.avatar}`}
+                    alt="Foto usuario"
+                />
+                <h3 className="doctor-name">Doctor: {doctor.username}</h3>
+                <p className="doctor-skill">Especialidad {doctor.Name}</p>
                 <p className="doctor-rating">
                     Media de valoraciones {doctor.averageRating}
                 </p>
@@ -20,27 +22,3 @@ export const CardDoctor = ({ doctor }) => {
         </>
     );
 };
-
-
-const staticPath = import.meta.env.VITE_BACKEND_STATIC;
-export const CardDoctor = ({doctor}) => {
-  /* console.log('Esto es lo que tiene la CardDoctor para pintar de doctores',doctor);
-  console.log('Esto es lo que tiene Cardoctor doctores.name',doctor.username); */
- /* const entryPhotoPath = `${staticPath}/a/${doctor.userId}/${doctor.id}`; */
- console.log(`${staticPath}/avatars/${doctor.userId}/${doctor.avatar}`);
- 
-  
-  return (
-    <>
-    <li className="card-doctor-inicio">
-    <img src={`${staticPath}/avatars/${doctor.userId}/${doctor.avatar}`} alt="Foto usuario" />
-    <h3 className="doctor-name">Doctor: {doctor.username}</h3>
-    <p className="doctor-skill">Especialidad {doctor.Name}</p>
-    <p className="doctor-rating">Media de valoraciones {doctor.averageRating}</p>
-    </li>
-    
-      
-    </>
-  )
-}
-
--- client/src/components/forms/ConsultForm.jsx
+++ client/src/components/forms/ConsultForm.jsx
@@ -12,64 +12,79 @@ import { toast } from 'react-toastify';
 import { ImageInput } from './ImageInput.jsx';
 
 export const ConsultForm = () => {
-	const { token } = useAuth();
-	const { info, previews, errors, validate, handleChange } = useForm();
-	const [isLoading, setIsLoading] = useState(false);
-	const navigate = useNavigate();
+    const { token } = useAuth();
+    const { info, previews, /*errors,*/ validate, handleChange } = useForm();
+    const [isLoading, setIsLoading] = useState(false);
+    const navigate = useNavigate();
 
-    
+    const handleSubmit = async (e) => {
+        try {
+            e.preventDefault();
 
+            validate(newConsultSchema);
 
-	const handleSubmit = async (e) => {
-		try {
-			e.preventDefault(); 
-            
-			validate(newConsultSchema);
+            setIsLoading(true);
+            const { message /*, data*/ } = await newConsultService(info, token);
 
-			setIsLoading(true);
-			const { message, data } = await newConsultService(info, token);
+            const params = new URLSearchParams({
+                type: 'success',
+                message,
+            });
 
-			const params = new URLSearchParams({
-				type: 'success',
-				message,
-			});
-
-			setTimeout(() => {
-				setIsLoading(false);
-				navigate(`/?${params.toString()}`);
-			}, 3000);
-            
-            
-		} catch (error) {
-			setIsLoading(false);
-			toast.error(error.message || 'Error al registrar la consulta');
-		}
-        
-	};
-	return (
-		<Form className='new-consult-form' handleSubmit={handleSubmit}>
-			<Input label='title' name='title' value={info.title} handleChange={handleChange}/>
-			<Input label='skillId' type="number" name='skillId' value={info.skillId} handleChange={handleChange}/>
-            <Input label='gravedad' name='gravedad' value={info.gravedad} handleChange={handleChange} />
-            <Input label='doctorId' name='doctorId' value={info.doctorId} handleChange={handleChange} />
-			<Input
-				label='Description'
-				type='textarea'
-				name='description'
-				value={info.description}
+            setTimeout(() => {
+                setIsLoading(false);
+                navigate(`/?${params.toString()}`);
+            }, 3000);
+        } catch (error) {
+            setIsLoading(false);
+            toast.error(error.message || 'Error al registrar la consulta');
+        }
+    };
+    return (
+        <Form className="new-consult-form" handleSubmit={handleSubmit}>
+            <Input
+                label="title"
+                name="title"
+                value={info.title}
                 handleChange={handleChange}
-			/>
+            />
+            <Input
+                label="skillId"
+                type="number"
+                name="skillId"
+                value={info.skillId}
+                handleChange={handleChange}
+            />
+            <Input
+                label="gravedad"
+                name="gravedad"
+                value={info.gravedad}
+                handleChange={handleChange}
+            />
+            <Input
+                label="doctorId"
+                name="doctorId"
+                value={info.doctorId}
+                handleChange={handleChange}
+            />
+            <Input
+                label="Description"
+                type="textarea"
+                name="description"
+                value={info.description}
+                handleChange={handleChange}
+            />
 
-			<ImageInput label='Image' name='images' previews={previews} />
-			<Button
-				id='register'
-				className='submit'
-				type='submit'
-				isLoading={isLoading}
-			>
-				<Icon name='send' />
-				<span className='text'>Consultar</span>
-			</Button>
-		</Form>
-	);
-}
+            <ImageInput label="Image" name="images" previews={previews} />
+            <Button
+                id="register"
+                className="submit"
+                type="submit"
+                isLoading={isLoading}
+            >
+                <Icon name="send" />
+                <span className="text">Consultar</span>
+            </Button>
+        </Form>
+    );
+};
