cdd157f78620dea8df5eb5c12875b4fc32ce8e21
luberrima <lucia.beatriz.franco@gmail.com>
2025-03-03 20:02:56 +0100

    Update CardDoctor.jsx

--- client/src/components/Landing/CardDoctor.jsx
+++ client/src/components/Landing/CardDoctor.jsx
@@ -1,26 +1,4 @@
 import React from 'react';
-import avatardefault from '../../assets/avatar-default.png';
-//import { Icon } from '../Icon.jsx';
-
-
-export const CardDoctor = ({ doctor }) => {
-    /* console.log('Esto es lo que tiene la CardDoctor para pintar de doctores',doctor);
-  console.log('Esto es lo que tiene Cardoctor doctores.name',doctor.username); */
-
-    return (
-        <>
-            <li className="card-doctor-inicio">
-                <img src={avatardefault} alt="avatar-default" />
-                <h3 className="doctor-name">{doctor.username}</h3>
-                <p className="doctor-skill">{doctor.skillId}</p>
-                <p className="doctor-rating">
-                    Media de valoraciones {doctor.averageRating}
-                </p>
-            </li>
-        </>
-    );
-};
-
 
 const staticPath = import.meta.env.VITE_BACKEND_STATIC;
 export const CardDoctor = ({doctor}) => {
