1928063565f4e5338230ec042652cca44a7bc26a
Javier Navarro <javinavarrorobles@gmail.com>
2025-03-04 16:59:23 +0100

    modificacion para las estrellas y la pagina de todos los doctores

--- client/src/components/Landing/CardDoctor.jsx
+++ client/src/components/Landing/CardDoctor.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import { Icon } from '../Icon.jsx'
-import { Link } from 'react-router-dom';
+import { Estrellas } from '../Estrellas.jsx';
+
 
 
 const staticPath = import.meta.env.VITE_BACKEND_STATIC;
 export const CardDoctor = ({ doctor }) => {
-    /* console.log('Esto es lo que tiene la CardDoctor para pintar de doctores',doctor);
-  console.log('Esto es lo que tiene Cardoctor doctores.name',doctor.username); */
- /* const entryPhotoPath = `${staticPath}/a/${doctor.userId}/${doctor.id}`; */
-
  
   
   return (
@@ -18,7 +14,8 @@ export const CardDoctor = ({ doctor }) => {
                         <img src={`${staticPath}/avatars/${doctor.userId}/${doctor.avatar}`} alt="Foto usuario" /></a>
     <h3>Doctor: {doctor.username}</h3>
     <p>Especialidad {doctor.Name}</p>
-    <p>Media de valoraciones {doctor.averageRating}</p>
+    <p>Media de valoraciones: </p>
+        <Estrellas rating={doctor.averageRating}/>
     </li>
     
       
