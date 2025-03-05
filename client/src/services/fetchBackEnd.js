c2e66e720fbc859c75b18a6efa81bc7b16751a89
lucia.franco <lucia.beatriz.franco@gmail.com>
2025-03-05 17:24:55 +0100

    cambios clase problema git

--- client/src/services/fetchBackEnd.js
+++ client/src/services/fetchBackEnd.js
@@ -10,9 +10,7 @@ export const getOwnUserService = async (token) => {
 };
 
 export const getAllDoctorsService = async () => {
-
     console.log('estoy dentro de getAllDoctorService');
-
 
     const response = await fetch(`${backEndPath}/users/doctors`);
 
@@ -20,53 +18,44 @@ export const getAllDoctorsService = async () => {
 
     if (!response.ok) throw new Error(message);
 
-
-    
-	return data;
-
+    return data;
 };
 
 export const getAllConsultasService = async (token) => {
+    const response = await fetch(`${backEndPath}/users/profile`, {
+        headers: {
+            Authorization: `${token}`,
+        },
+    });
 
-	const response = await fetch(`${backEndPath}/users/profile`,{
-		
-		headers: {
-			Authorization: `${token}`
-		}
-	});
-	
-	const { message, data } = await response.json();
+    const { message, data } = await response.json();
 
-	if (!response.ok) throw new Error(message);
-    
-	return data;
+    if (!response.ok) throw new Error(message);
 
+    return data;
 };
 
 export const getAllConsulNoAsigService = async (token) => {
-
     console.log('Esta es la ruta del fech', `${backEndPath}/consultations`);
-    
 
-	const response = await fetch(`${backEndPath}/consultations`,{
-		
-		headers: {
-			Authorization: `${token}`
-		}
-	});
+    const response = await fetch(`${backEndPath}/consultations`, {
+        headers: {
+            Authorization: `${token}`,
+        },
+    });
     console.log('Que devuelve respose de consul no asignadas', response);
-	
 
-	const { message, data } = await response.json();
+    const { message, data } = await response.json();
 
-	if (!response.ok) throw new Error(message);
+    if (!response.ok) throw new Error(message);
 
+    console.log(
+        'esto es lo que retorna el getAllConsultas No asginadas Service',
+        data
+    );
 
-    console.log('esto es lo que retorna el getAllConsultas No asginadas Service', data); 
-    
-	return data;
+    return data;
 };
-
 
 export const getConsultationDetailService = async (consultationId, token) => {
     console.log('token en service:', token);
@@ -97,53 +86,47 @@ export const getConsultationDetailService = async (consultationId, token) => {
     }
 };
 
-
 export const newConsultService = async (info, token) => {
-
     /* console.log('Esto es lo que llega al server',info,token); */
-   
 
-	const formData = new FormData();
-	formData.append('title', info.title || '');
-	formData.append('skillId', info.skillId || '');
+    const formData = new FormData();
+    formData.append('title', info.title || '');
+    formData.append('skillId', info.skillId || '');
     formData.append('gravedad', info.gravedad || '');
     formData.append('doctorId', info.doctorId || '');
-	formData.append('description', info.description || '');
-	if (info.img1) formData.append('img1', info.img1);
-	if (info.img2) formData.append('img2', info.img2);
-	if (info.img3) formData.append('img3', info.img3);
+    formData.append('description', info.description || '');
+    if (info.img1) formData.append('img1', info.img1);
+    if (info.img2) formData.append('img2', info.img2);
+    if (info.img3) formData.append('img3', info.img3);
     if (info.img4) formData.append('img4', info.img4);
 
-	const response = await fetch(`${backEndPath}/new-consultation`, {
-		method: 'POST',
-		body: formData,
-		headers: {
-			Authorization: `${token}`,
-		},
-	});
-	const { message, data } = await response.json();
+    const response = await fetch(`${backEndPath}/new-consultation`, {
+        method: 'POST',
+        body: formData,
+        headers: {
+            Authorization: `${token}`,
+        },
+    });
+    const { message, data } = await response.json();
 
-	if (!response.ok) throw new Error(message);
-	console.log("Valor de message",message);
-	console.log("Valor de data");
+    if (!response.ok) throw new Error(message);
+    console.log('Valor de message', message);
+    console.log('Valor de data');
 
-	return { message, data };
+    return { message, data };
 };
 
-export const getDoctorProfileService = async (id,token) => {
+export const getDoctorProfileService = async (id, token) => {
     const response = await fetch(`${backEndPath}/users/doctors/${id}`, {
         headers: {
             Authorization: `${token}`,
         },
     });
-	const { message, data } = await response.json();
+    const { message, data } = await response.json();
 
-	if (!response.ok) throw new Error(message);
+    if (!response.ok) throw new Error(message);
 
     return { message, data };
-};
-
-
 };
 
 export const registerUserService = async (userData) => {
@@ -167,4 +150,3 @@ export const registerDoctorService = async (doctorData) => {
     if (!response.ok) throw new Error(message);
     return message;
 };
-
