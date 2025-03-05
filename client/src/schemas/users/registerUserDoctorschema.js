dd28acfce92c914dd3e67cf0dcc36a2d52405bbb
lucia.franco <lucia.beatriz.franco@gmail.com>
2025-03-05 17:53:41 +0100

    schemas

---
+++ client/src/schemas/users/registerUserDoctorschema.js
@@ -0,0 +1,16 @@
+import joi from 'joi';
+import { joiErrorMessages } from '../joiErrorMessages.js';
+export const newDoctorSchema = joi.object({
+    username: joi.string().min(3).max(50).required().messages(joiErrorMessages),
+    email: joi.string().email().required().messages(joiErrorMessages),
+    password: joi
+        .string()
+        .pattern(
+            /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+        )
+        .required()
+        .messages(joiErrorMessages),
+    collegeNumber: joi.string().required().messages(joiErrorMessages),
+    dateOfCollege: joi.date().required().messages(joiErrorMessages),
+    skillId: joi.number().integer().required().messages(joiErrorMessages),
+});
